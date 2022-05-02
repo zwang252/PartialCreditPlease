b7a98923d9ce22e5effcc4495bc374e13debb377
zane0916 <zwang252@binghamton.edu>
2022-05-02 13:32:24 -0400

    made chessboard

---
+++ Chessboard.cpp
@@ -0,0 +1,24 @@
+#include "Square.cpp"
+
+class Chessboard {
+    public:
+        Chessboard() {
+            for (int col = 0; col < 8; col++) {
+                for (int row = 0; row < 8; row++) {
+                    board[col][row].setCol(col);
+	                cboard[col][row].setRow(row);
+	            }
+            }
+        }
+        void display() {
+            for (int row = 8; row > 0; row--) {
+                for (int column = 0; column < 8; column++) {
+	                cout << board[column][row - 1] << "  ";
+	            }
+	        cout << endl << endl;
+            }
+        }
+
+    private:
+        Square board[8][8];
+}
--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ all:	PartialCreditPlease
 PartialCreditPlease:	main.o
 	g++ main.o -o pcpkt
 
-main.o:	main.cpp Square.o Knight.o
+main.o:	main.cpp Square.o Knight.o Chessboard.o
 	g++ -c main.cpp
 
 Square.o:	Square.cpp
@@ -11,6 +11,9 @@ Square.o:	Square.cpp
 
 Knight.o:	Knight.cpp
 	g++ -c Knight.cpp
+
+Chessboard.o:	Chessboard.cpp
+	g++ -c Chessboard.cpp
 
 clean:
 	rm *.o pcpkt
