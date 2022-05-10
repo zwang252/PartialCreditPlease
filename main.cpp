ec6496bcf8fc13dfddff1d9fb2f83b7915e87c87
Stanley <slin111@binghamton.edu>
2022-05-10 17:05:30 -0400

    added timer

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "Knight.h"
+#include <chrono>
 
 int main(int argc, char *argv[]) {
 
@@ -19,17 +20,27 @@ int main(int argc, char *argv[]) {
     int var1 = (int) (tile[0] - 65);
     int var2 = (int) (tile[1] - 48);
     lancelot.setLocation(*chessboard.get(var1, var2 - 1));
+
+    auto start = chrono::steady_clock::now();
         
     //Backtrack
     if (algChoice == 1) {
         vector<Square> temp2;
         vector<Square> temp3 = lancelot.moveBackT(temp2);
         chessboard.displayResults(temp3);
+
+        auto end = chrono::steady_clock::now();
+        auto diff = end - start;
+        cout << chrono::duration<double, std::ratio<60>>(diff).count() << " mins" << endl; 
     }
     //Warns
     if (algChoice == 2) {
         vector<Square> temp = lancelot.moveWarns();
         chessboard.displayResults(temp);
+
+        auto end = chrono::steady_clock::now();
+        auto diff = end - start;
+        cout << chrono::duration<double, std::milli>(diff).count() << " ms" << endl; 
     }
 
 
